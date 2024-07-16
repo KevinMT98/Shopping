16c51869bc9be3c12bef6b63b6d406eb4d4d2606
Kevin Montaño <Kevin Montao@DESKTOP-IKA0HN7>
2024-07-16 00:22:03 -0500

    prueba2

--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -1,15 +1,19 @@
+
 using Microsoft.EntityFrameworkCore;
 using Shopping.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<DataContext>(o =>
 {
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnetion"));
 });
+
 
 var app = builder.Build();
 
