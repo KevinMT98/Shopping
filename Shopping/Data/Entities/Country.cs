99e9df1ddbbda77e1b69feee41013c998eb8fb46
Kevin Montaño <Kevin Montao@DESKTOP-IKA0HN7>
2024-07-15 20:30:59 -0500

    Inicio Shopping

---
+++ Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]  // cantidad de caracteres del campo
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]       // no permite ingresar nulo en la propiedas (data anotation)
+        public string? Name { get; set; }
+
+
+    }
+}
